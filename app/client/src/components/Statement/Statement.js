232c07224dbe876e09a5aff3ea744839fabe507f
Ryan McKenzie <ryanjm88@gmail.com>
2018-07-05 18:39:01 -0400

    statement.js update

---
+++ app/client/src/components/Statement/Statement.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Component } from 'react';
+
+class Statement extends Component{
+    render()    {
+        return  (
+            <div>
+                <div class="card" id="expenseTypes">
+                    <button type="button" class="btn btn-primary" id="rent">Rent/Mortgage</button>
+                    <button type="button" class="btn btn-primary" id="util">Utilities</button>
+                    <button type="button" class="btn btn-primary" id="car">Car/Transit</button>
+                    <button type="button" class="btn btn-primary" id="food">Food/Dining</button>
+                    <button type="button" class="btn btn-primary" id="credit">Credit Cards</button>
+                    <button type="button" class="btn btn-primary" id="loans">Loans</button>
+                    <button type="button" class="btn btn-primary" id="medical">Medical</button>
+                    <button type="button" class="btn btn-primary" id="misc">Misc.</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Statement;
---
+++ app/client/src/components/StatementInfo/StatementInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StatementInfo = props => (
+    <div class="card">
+        <h4>Bill Name: </h4>
+        <h4>Category: </h4>
+        <h4>Due Date: </h4>
+    </div>
+);
+
+export default StatementInfo;
