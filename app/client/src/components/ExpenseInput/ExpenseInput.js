7d47a4f23880ae81a772acaa10a14547c8fd19e7
John Waymire <waymire68@yahoo.com>
2018-07-06 16:20:51 -0400

    created input/select components

--- app/client/src/components/ExpenseInput/ExpenseInput.js
+++ app/client/src/components/ExpenseInput/ExpenseInput.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import Input from './components/Input';
+import Input from '../Input/Input';
+import CustomSelect from '../CustomSelect/CustomSelect';
+import CheckBox from '../CheckBox/CheckBox';
 
 class ExpenseInput extends Component {
   constructor(props) {
@@ -24,89 +26,53 @@ class ExpenseInput extends Component {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label>Expense Category</label>
-            <select
-              className="custom-select"
-              name="expenseCategory"
-              value={this.state.value}
-              onChange={this.handleInputChange}
-            >
-              <option value="Rent/Mortgage">Rent/Mortgage</option>
-              <option value="Utilities">Utilities</option>
-              <option value="Car/Transportation">Car/Transportation</option>
-              <option value="Food/Dining">Food/Dining</option>
-              <option value="Credit Cards">Credit Cards</option>
-              <option value="Loans">Loans</option>
-              <option value="Medical/Health">Medical/Health</option>
-              <option value="Other">Other</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label>Bill Name</label>
-            <input
-              name="billName"
-              type="text"
-              value={this.state.billName}
-              onChange={this.handleInputChange}
-              className="form-control"
-            />
-          </div>
-          <div className="form-group">
-            <label for="">Amount/Monthly Average</label>
-            <input
-              name="amount"
-              type="text"
-              value={this.state.amount}
-              onChange={this.handleInputChange}
-              placeholder="00.00"
-              className="form-control"
-            />
-          </div>
-          <div className="form-group">
-            <label for="">Due Date</label>
-            <input
-              name="dueDate"
-              type="text"
-              value={this.state.dueDate}
-              onChange={this.handleInputChange}
-              placeholder="MM/DD"
-              className="form-control"
-            />
-          </div>
-          <div className="form-group">
-            <label for="acctPaidFrom">Account Paid From</label>
-            <input
-              name="acctPaidFrom"
-              type="text"
-              value={this.state.acctPaidFrom}
-              onChange={this.handleInputChange}
-              placeholder="Bank/Credit Card"
-              className="form-control"
-            />
-          </div>
-          <div className="form-check">
-            <input
-              name="autoPay"
-              type="checkbox"
-              value={this.state.autoPay}
-              onChange={this.handleInputChange}
-              className="form-check-input"
-            />
-            <label className="form-check-label">Auto Pay</label>
-          </div>
-          <br />
-          <input
-            className="btn btn-primary btn-lg"
-            type="submit"
-            value="Submit"
+          <CustomSelect
+            name="expenseCategory"
+            value={this.state.value}
+            onChange={this.handleInputChange}
           />
+          <label>Bill Name</label>
+          <Input
+            name="billName"
+            type="text"
+            value={this.state.billName}
+            onChange={this.handleInputChange}
+          />
+          <label>Amount/Monthly Average</label>
           <Input
             name="amount"
             type="text"
             value={this.state.amount}
             onChange={this.handleInputChange}
             placeholder="00.00"
+          />
+          <label>Due Date</label>
+          <Input
+            name="dueDate"
+            type="text"
+            value={this.state.dueDate}
+            onChange={this.handleInputChange}
+            placeholder="MM/DD"
+          />
+          <label>Account Paid From</label>
+          <Input
+            name="acctPaidFrom"
+            type="text"
+            value={this.state.acctPaidFrom}
+            onChange={this.handleInputChange}
+            placeholder="Bank/Credit Card"
+          />
+          <CheckBox
+            name="autoPay"
+            type="checkbox"
+            value={this.state.autoPay}
+            onChange={this.handleInputChange}
+          />
+          <br />
+          <input
+            className="btn btn-warning btn-lg"
+            type="submit"
+            value="Submit"
           />
         </form>
       </div>
