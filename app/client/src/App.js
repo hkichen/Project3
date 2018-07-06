46ee8b2f1377e3d63628003080946a0db2a54f54
Ryan McKenzie <ryanjm88@gmail.com>
2018-07-05 20:20:55 -0400

    statement second push

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Statement from './components/Statement/Statement';
 
 // import all the pages and components relevant
 
